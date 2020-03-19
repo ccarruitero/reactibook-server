43314c148b9fbe19733bbf968fb81bad6c67302c
César Carruitero <ccarruitero@protonmail.com>
2020-03-19 16:46:36 -0500

    add route for validate token

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const User = require('../models/user');
+const auth = require('../middlewares/auth');
 
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
@@ -12,6 +13,10 @@ router.post('/', async (req, res) => {
   } else {
     res.status(422).json({ error: 'Invalid email or password' });
   }
+});
+
+router.post('/introspect', auth, (req, res) => {
+  res.json({});
 });
 
 module.exports = router;
